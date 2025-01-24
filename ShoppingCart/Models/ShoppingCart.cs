ebb07c7359ee4b0114db9101a66c79a7a5508c16
bankuti.laszlo <bankuti.laszlo@BURG-1E06-04>
2025-01-24 08:49:16 +0100

    all done

--- ShoppingCart/Models/ShoppingCart.cs
+++ ShoppingCart/Models/ShoppingCart.cs
@@ -12,6 +12,28 @@
         public int ProductCount => _products.Count;
 
         //TODO Készítse el a ShoppingCart osztályban azokat a metódusokat, amelyekkel sikeresen lefutnak a tesztesetek!
+        public void AddProduct(string name, double price)
+        {
+            var product = new Product(name, price);
+            _products.Add(product);
+        }
+        public void RemoveProduct(string name)
+        {
+            var product = _products.FirstOrDefault(p => p.Name == name);
+            if (product == null)
+            {
+                throw new InvalidOperationException("Product not found in the cart.");
+            }
+            _products.Remove(product);
+        }
+        public double GetTotalPrice()
+        {
+            return _products.Sum(p => p.Price);
+        }
 
+        public List<Product> GetProducts()
+        {
+            return new List<Product>(_products);
+        }
     }
 }
